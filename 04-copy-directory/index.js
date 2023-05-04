381f408877e546b812da85b589bac76c8381d944
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-04 13:30:50 +0300

    feat: implement 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
 
+const copyDir = () => {
+
+  const originalFolder = path.join(__dirname, 'files');
+  const copyFolder = path.join(__dirname, 'files-copy');
+
+  fsPromises.mkdir(copyFolder, { recursive: true }).then(function () {
+    console.log('New folder was created successfully');
+  }).catch(function () {
+    console.log('Failed to create new folder');
+  });
+
+  fs.readdir(path.join(originalFolder), (error, files) => {
+    files.forEach(file => {
+      fs.copyFile(path.join(originalFolder, file), path.join(copyFolder, file), error =>{
+        if (error) {
+          console.log(error);
+        }
+        console.log(`File ${file} was copied successfully`);
+      });
+    });
+  });
+};
+
+copyDir();
