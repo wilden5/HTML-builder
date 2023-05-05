1da7cb12b9bb8b7e3b0ce33ee48aa478ef4697b2
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-05 14:33:38 +0300

    fix: handle the case when we add/remove files in the existing files-copy folder for 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,22 +7,24 @@ const copyDir = () => {
   const originalFolder = path.join(__dirname, 'files');
   const copyFolder = path.join(__dirname, 'files-copy');
 
-  fsPromises.mkdir(copyFolder, { recursive: true }).then(function () {
-    console.log('New folder was created successfully');
-  }).catch(function () {
-    console.log('Failed to create new folder');
-  });
+  fsPromises.access(copyFolder)
+    .then(() => fsPromises.rm(copyFolder, { recursive: true }))
+    .catch(() => {})
+    .then(() => fsPromises.mkdir(copyFolder, { recursive: true }))
+    .then(() => {
+      console.log('New folder was created successfully');
 
-  fs.readdir(path.join(originalFolder), (error, files) => {
-    files.forEach(file => {
-      fs.copyFile(path.join(originalFolder, file), path.join(copyFolder, file), error =>{
-        if (error) {
-          console.log(error);
-        }
-        console.log(`File ${file} was copied successfully`);
+      fs.readdir(path.join(originalFolder), (error, files) => {
+        files.forEach(file => {
+          fs.copyFile(path.join(originalFolder, file), path.join(copyFolder, file), error =>{
+            if (error) {
+              console.log(error);
+            }
+            console.log(`File ${file} was copied successfully`);
+          });
+        });
       });
     });
-  });
 };
 
 copyDir();
